f6b7bdccf40133c752e03a5e874f6e9b5a4141e6
IxtiyorDev <ixtiyoryarashov14011401@gmail.com>
2022-11-25 18:39:03 +0500

    first commit

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 
-
 async def on_startup(dispatcher):
     # Birlamchi komandalar (/star va /help)
     await set_default_commands(dispatcher)
